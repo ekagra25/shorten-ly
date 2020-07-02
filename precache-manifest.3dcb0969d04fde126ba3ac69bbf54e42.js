c230c515d5bbb1f26f8e3b9b96d14570307592cb
Ekagra Midha <ekagra99@yahoo.com>
2020-07-02 09:28:57 +0530

    Initial Commit

---
+++ precache-manifest.3dcb0969d04fde126ba3ac69bbf54e42.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a4e8326b12b6ab871244445f89e396",
+    "url": "/shorten-ly/index.html"
+  },
+  {
+    "revision": "c340488e7453337dda54",
+    "url": "/shorten-ly/static/css/2.658b26d9.chunk.css"
+  },
+  {
+    "revision": "ba588f888efc3f187b2f",
+    "url": "/shorten-ly/static/css/main.a8c498fd.chunk.css"
+  },
+  {
+    "revision": "c340488e7453337dda54",
+    "url": "/shorten-ly/static/js/2.713f97ee.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/shorten-ly/static/js/2.713f97ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba588f888efc3f187b2f",
+    "url": "/shorten-ly/static/js/main.bd231f33.chunk.js"
+  },
+  {
+    "revision": "a46b113a1c5d92c34b8d",
+    "url": "/shorten-ly/static/js/runtime-main.43c154dd.js"
+  },
+  {
+    "revision": "ff7ac8d3ed7f4736ad0471bac19668d5",
+    "url": "/shorten-ly/static/media/background.ff7ac8d3.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/shorten-ly/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/shorten-ly/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/shorten-ly/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/shorten-ly/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/shorten-ly/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "aad09bad117129f7cfac68acd706c032",
+    "url": "/shorten-ly/static/media/logo.aad09bad.png"
+  },
+  {
+    "revision": "f4bfaac008d50c101ae060737eb188ff",
+    "url": "/shorten-ly/static/media/title.f4bfaac0.png"
+  }
+]);
